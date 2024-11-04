a01ced48a4a7978d08f4243d614d6b6f7dbb1736
M-Usman-Shafique <hafizusmanshafiq@gmail.com>
2024-11-04 11:10:36 +0500

    Union type

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import "./App.css";
 import Greet from "./components/Greet";
+import Status from "./components/Status";
 import Users from "./components/Users";
 
 export default function App() {
-const usersList = [
-  {
-    first: "Noman",
-    last: "Shafiq"
-  },
-  {
-    first: "Salman",
-    last: "Shafiq"
-  },
-  {
-    first: "Waseem",
-    last: "Shah"
-  },
-]
+  const usersList = [
+    {
+      first: "Noman",
+      last: "Shafiq",
+    },
+    {
+      first: "Salman",
+      last: "Shafiq",
+    },
+    {
+      first: "Waseem",
+      last: "Shah",
+    },
+  ];
   return (
     <>
       <Greet name="Usman" msgs={12} isLogin={false} />
-      <Users users={usersList}/>
+      <Users users={usersList} />
+      <Status status="success" />
     </>
   );
 }
